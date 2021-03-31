0c1f413247bbf7cd6b69f530ff44336fd590046f
yongliang.lang <yongliang.lang@bjrxtd.com>
2021-03-31 22:26:43 +0800

    clear cached

--- app/src/main/java/com/uratio/demop/wave/WaveActivity.java
+++ app/src/main/java/com/uratio/demop/wave/WaveActivity.java
@@ -4,12 +4,16 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.uratio.demop.R;
 
 public class WaveActivity extends AppCompatActivity {
     private WaveBallProgress waveBall;
     private SineWave sineWave;
+    private View view;
 
     private Handler handler = new Handler() {
         @Override
@@ -36,5 +40,12 @@ public class WaveActivity extends AppCompatActivity {
         sineWave = findViewById(R.id.sine_wave);
 
         handler.sendEmptyMessage(0);
+
+        view = findViewById(R.id.view);
+        view.setAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_cycle_interpolator));
+    }
+
+    public void ClickView(View view) {
+        view.getAnimation().start();
     }
 }
