c97ba0a3fa6b360eec8fe493fcac127e7103ebba
yongliang.lang <yongliang.lang@bjrxtd.com>
2021-05-26 15:06:29 +0800

    修改和新增

--- app/src/main/java/com/uratio/demop/Test.java
+++ app/src/main/java/com/uratio/demop/Test.java
@@ -1,35 +1,5 @@
 package com.uratio.demop;
 
 public class Test {
-
-/*In com.manpower.rrb:5.0.2:86. * com.manpower.rrb.ui.activity.home.business.OrderPayActivity has leaked: * GC ROOT static com.manpower.rrb.c.c.b.g *
- leaks com.manpower.rrb.ui.activity.home.business.OrderPayActivity instance * Retaining: 417 KB. * Reference Key:
- 48ac7e5b-3ddf-4270-a061-00b1f95cfd55 * Device: Xiaomi xiaomi Redmi 5 Plus vince * Android Version: 7.1.2 API: 25 LeakCanary: 1.5.4 74837f0 *
- Durations: watch=645233ms, gc=178ms, heap dump=3106ms, analysis=78934ms * Details: * Class com.manpower.rrb.c.c.b | static g = com.manpower.rrb.ui
- .activity.home.business.OrderPayActivity@321850192 (0x132f0b50) | static a = com.tencent.mm.sdk.openapi.WXApiImplV10@326704840 (0x13791ec8) |
- static $classOverhead = byte[144]@325879137 (0x136c8561) * Instance of com.manpower.rrb.ui.activity.home.business.OrderPayActivity | static
- $classOverhead = byte[4280]@318078977 (0x12f58001) | aliPay = null | b = 167.12 | btnPay = null | c = com.manpower.rrb.c.b.a@326704480
- (0x13791d60) | d = com.manpower.rrb.c.c.b@326286976 (0x1372be80) | e = null | f = null | g = null | h = false | i = 9 | j = java.lang
- .String@326705008 (0x13791f70) | k = com.manpower.rrb.presenter.b.t@326711328 (0x13793820) | l = com.manpower.rrb.model.bean
- .OrderPayInfo@327201056 (0x1380b120) | m = 0 | n = com.manpower.rrb.ui.activity.home.business.OrderPayActivity$1@323095168 (0x13420a80) |
- tvAllCost = null | tvCountDown = null | tvServiceContent = null | wxPay = null | a = com.manpower.rrb.ui.activity.home.business
- .OrderPayActivity@321850192 (0x132f0b50) | b = android.support.v7.app.c@326607952 (0x1377a450) | c = com.manpower.rrb.ui.widget.dialog.e@326702632
-  (0x13791628) | d = 1 | e = 1000 | f = 1542613792997 | g = com.manpower.rrb.ui.activity.home.business.OrderPayActivity_ViewBinding@326285280
-  (0x1372b7e0) | mDelegate = android.support.v7.app.g@325305248 (0x1363c3a0) | mResources = null | mThemeId = 2131427386 | mCreated = true |
-  mFragments = android.support.v4.app.n@323416464 (0x1346f190) | mHandler = android.support.v4.app.FragmentActivity$1@323095232 (0x13420ac0) |
-  mNextCandidateRequestIndex = 0 | mPendingFragmentActivityResults = android.support.v4.k.r@323459008 (0x134797c0) | mReallyStopped = true |
-  mRequestedPermissionsFromFragment = false | mResumed = false | mRetaining = false | mStopped = true | mStartedActivityFromFragment = false |
-  mStartedIntentSenderFromFragment = false | mExtraDataMap = android.support.v4.k.q@323458096 (0x13479430) | mLifecycleRegistry = android.arch
-  .lifecycle.d@323095328 (0x13420b20) | mActionBar = null | mActionModeTypeStarting = 0 | mActivityInfo = android.content.pm.ActivityInfo@325304672
-   (0x1363c160) | mActivityTransitionState = android.app.ActivityTransitionState@325196032 (0x13621900) | mApplication = com.manpower.rrb
-   .CompatApplication@316048032 (0x12d682a0) | mCalled = true | mChangeCanvasToTranslucent = false | mChangingConfigurations = false |
-   mCheckForDragging = false | mComponent = android.content.ComponentName@323612912 (0x1349f0f0) | mConfigChangeFlags = 0 | mCurrentConfig =
-   android.content.res.Configuration@324721128 (0x135ad9e8) | mDecor = null | mDefaultKeyMode = 0 | mDefaultKeySsb = null | mDestroyed = true |
-   mDoReportFullyDrawn = false | mDragging = false | mEatKeyUpEvent = false | mEmbeddedID = null | mEnableDefaultActionBarUp = false |
-   mEnterTransitionListener = android.app.SharedElementCallback$1@1893075752 (0x70d60b28) | mExitTransitionListener = android.app
-   .SharedElementCallback$1@1893075752 (0x70d60b28) | mFinished = true | mFragments = android.app.FragmentController@323416400 (0x1346f150) |
-   mHandler = android.os.Handler@323095392 (0x13420b60) | mHasCurrentPermissionsRequest = false | mIdent = 235697184 | mInstanceTracker = android
-   .os.StrictMode$InstanceTracker@323416416 (0x1346f160) | mInstrumentation = android.app.Instrumentation@315682672 (0x12d0ef70) | mIntent =
-   android.content.Intent@327320048 (0x138281f0) | mInterceptor = miui.contentcatcher.InterceptorProxy@323416864 (0x1346f320) |
-   mIsInPictureInPictureMode = false | mLastDraging = false | mLastNonConfigurationInstances = null | mMainThread = android.app.Act*/
+//修改test文件
 }
