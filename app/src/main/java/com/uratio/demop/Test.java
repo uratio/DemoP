a47e374d6d7a16d96f1d8653e92e74f8f57d89c2
yongliang.lang <yongliang.lang@bjrxtd.com>
2021-05-26 15:10:56 +0800

    修改、删除、新增

--- app/src/main/java/com/uratio/demop/Test.java
+++ app/src/main/java/com/uratio/demop/Test.java
@@ -1,5 +1,5 @@
 package com.uratio.demop;
 
 public class Test {
-    //修改test文件
+    //修改test文件.
 }
--- app/src/main/java/com/uratio/demop/TestNew3.java
+++ app/src/main/java/com/uratio/demop/TestNew3.java
@@ -4,5 +4,5 @@ package com.uratio.demop;
  * @author lang
  * @data 2021/5/26
  */
-public class TestNew2 {
+public class TestNew3 {
 }
