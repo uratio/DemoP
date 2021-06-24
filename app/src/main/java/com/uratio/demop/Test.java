ec82183d4d33abdd13fa67244b914ddbf1919794
yongliang.lang <yongliang.lang@bjrxtd.com>
2021-06-24 09:45:38 +0800

    测试新建分支代码

--- app/src/main/java/com/uratio/demop/Test.java
+++ app/src/main/java/com/uratio/demop/Test.java
@@ -1,5 +1,14 @@
 package com.uratio.demop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     //修改test文件.
+    public void main() {
+        String str = "";
+        str.isEmpty();
+        List<String> list = new ArrayList<>();
+        list.isEmpty();
+    }
 }
